90842f34dca698e0b5d3a681a40da9440f3cc425
Ryan Niebur <ryanryan52@gmail.com>
2009-05-29 00:43:52 -0700

    also set a priority number for the alternative in the distro config file. This sets it to 50 for debian, and 39 for ubuntu (just under firefox). This addresses the other half of the ubuntu diff.

--- debian/config/Debian.h
+++ debian/config/Debian.h
@@ -1,1 +1,3 @@
 #define DEBIAN_DISTRO_DEFAULT_HOMEPAGE "file:///usr/share/doc/midori/user/midori.html"
+#define DEBIAN_WWW_ALTERNATIVES_PRIORITY 50
+
--- debian/config/Ubuntu.h
+++ debian/config/Ubuntu.h
@@ -1,1 +1,3 @@
 #define DEBIAN_DISTRO_DEFAULT_HOMEPAGE "file:///usr/share/ubuntu-artwork/home/index.html"
+#define DEBIAN_WWW_ALTERNATIVES_PRIORITY 39
+
